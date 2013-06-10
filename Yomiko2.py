fc758e82aa90a657539cce39f688bcfae990c86b
Duncan Law-Green <git@kyubi.co.uk>
2013-06-10 16:33:29 +0100

    Copy base classes to Yomiko2.py

--- Yomiko2.py
+++ Yomiko2.py
@@ -18,6 +18,84 @@ app.config.from_object('config')
 
 print app.config['DATABASE_URI']
 
+# CLASS DEFINITION ============================================
 
+class Directory:
+    """Directory(path) -- instantiate data directory object"""
+    def __init__(self, Path):
+        self.path = Path
+
+    """scan() -- scans data directory for archive files"""
+    def scan():
+        pass
+
+class Archive:
+    """Archive() -- instantiate object for archive file"""
+    def __init__():
+        self.id=0
+        self.type=""
+        self.name=""
+        self.size=0
+        self.created=""
+        self.path=""
+        self.md5=""
+        self.images=[]
+
+    def getMetadata():
+        pass
+
+    def extractImage(n):
+        pass
+
+    def extractAll():
+        pass
+
+    def listImages():
+        pass
+
+    def clearCache():
+        pass
+
+class Image:
+    """Image() -- instantiate object for individual image"""
+    def __init__():
+        self.archiveid=0
+        self.imageid=0
+        self.filename=""
+        self.thumbnail=""
+        self.cached=False
+
+    def makeThumb():
+        pass
+
+    def getThumb():
+        pass
+
+    def showThumb():
+        pass
+
+    def getNext():
+        pass
+
+    def getPrev():
+        pass
+
+class Tagset:
+    """Tagset() -- instantiate object for set of image tags"""
+    self.archiveid=0
+    self.tagid=0
+    self.tag=""
+    self.text=""
+
+    def showTags():
+        pass
+
+    def addTag():
+        pass
+
+    def delTag():
+        pass
+
+# END CLASS DEFINITION ========================================   
 
 
