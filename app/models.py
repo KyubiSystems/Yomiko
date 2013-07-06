0e49a9d19c526f9c7c831608a8429fbe98763131
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-07-07 00:27:34 +0100

    Fix typo in models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.declarative import declarative.base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Text
 from datetime import datetime
 
