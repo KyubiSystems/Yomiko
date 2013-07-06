28697cb75e464298475464b89500d43195140c30
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-07-07 00:40:50 +0100

    Add correct imports for SQLAlchemy ORM

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
 from datetime import datetime
 
 # declare base model class
