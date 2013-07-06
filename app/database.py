5fe0961c95fc731802498e5ecb4a1ac79c3a5e84
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-07-07 00:49:48 +0100

    Change name of SQLite3 file to prevent clash

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+engine = create_engine('sqlite:////tmp/Yomiko.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
