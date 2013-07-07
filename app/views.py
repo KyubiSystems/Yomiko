0c1f898612ee88915fc205f76ccd10ec9e8e0ebf
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-07-07 21:54:05 +0100

    Fix typos in views

--- app/views.py
+++ app/views.py
@@ -6,22 +6,22 @@ from app import app
 def index():
     return render_template("index.html")
 
-@app.route('/list'):
+@app.route('/list')
 def plist():
     return render_template("list.html")
 
-@app.route('/tags'):
+@app.route('/tags')
 def tags():
     return render_template("tags.html")
 
-@app.route('/settings'):
+@app.route('/settings')
 def settings():
     return render_template("settings.html")
 
-@app.route('/title'):
+@app.route('/title')
 def title():
     return render_template("title.html")
 
-@app.route('/page'):
+@app.route('/page')
 def page():
     return render_template("page.html")
