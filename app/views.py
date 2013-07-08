22f516edc4afef95cb4ec06624fbea0acb66a41a
Duncan Law-Green <duncan.lawgreen@gmail.com>
2013-07-09 00:03:26 +0100

    Add error page handling

--- app/views.py
+++ app/views.py
@@ -25,3 +25,15 @@ def title():
 @app.route('/page')
 def page():
     return render_template("page.html")
+
+# Error handling
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_error(errror):
+    db.session.rollback()
+    return render_template("500.html"), 500
+
