d93a453ec478684056c7a966a9083e0024a905a6
Duncan Law-Green <git@kyubi.co.uk>
2014-02-03 14:28:09 +0000

    Add INPUT_PATH config, image extension filtering

--- app/dirReader.py
+++ app/dirReader.py
@@ -4,14 +4,23 @@ import zipfile
 import rarfile
 import os
 import fnmatch
-
-fileroot = '~/Downloads/'
-fileroot = os.path.expanduser(fileroot)
+from config import *
 
 zips = []
 rars = []
 
-for file in os.listdir(fileroot):
+# list of defined image extensions
+IMAGE_EXTS = ['.gif', '.GIF', '.png', '.PNG', '.jpg', '.JPG', '.jpeg', '.JPEG']
+
+# determine whether filename has image extension
+def isImage(file):
+    name, extension = os.path.splitext(file)
+    if extension in IMAGE_EXTS:
+        return True
+    else:
+        return False
+
+for file in os.listdir(INPUT_PATH):
     if fnmatch.fnmatch(file, '*.[Zz][Ii][Pp]'):
         zips.append(file)
 
@@ -20,15 +29,19 @@ for file in os.listdir(fileroot):
 
 for file in zips:
     print "File: "+file
-    myzip = zipfile.ZipFile(fileroot+file,'r')
+    myzip = zipfile.ZipFile(INPUT_PATH+file, 'r')
     zip_members = myzip.namelist()
+    zip_members = filter(lambda x: isImage(x) is True, zip_members)
     print zip_members
     print '------------------'
 
 for file in rars:
     print "File: "+file
-    myrar = rarfile.RarFile(fileroot+file,'r')
+    myrar = rarfile.RarFile(INPUT_PATH+file, 'r')
     rar_members = myrar.namelist()
+    rar_members = filter(lambda x: isImage(x) is True, rar_members)
     print rar_members
     print '------------------'
+
+
 
