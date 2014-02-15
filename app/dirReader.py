b3dc3518656ec371336d3b8298c512a93fc7d181
Duncan Law-Green <duncan.lawgreen@gmail.com>
2014-02-15 22:14:02 +0000

    Progress towards populating DB

--- app/dirReader.py
+++ app/dirReader.py
@@ -7,7 +7,9 @@ Yomiko Comics Reader
 import zipfile
 import rarfile
 import fnmatch
-from config import *
+import hashlib
+import split_tags as tag
+from models import *
 
 zips = []
 rars = []
@@ -24,6 +26,16 @@ def is_image(f):
     else:
         return False
 
+
+# Get MD5 checksum of input file
+def md5sum(filename, blocksize=65536):
+    hash = hashlib.md5()
+    with open(filename, "r+b") as f:
+        for block in iter(lambda: f.read(blocksize), ""):
+            hash.update(block)
+    return hash.hexdigest()
+
+
 # Generate thumbnail and save to cache
 def make_thumbnail(f):
     pass
@@ -33,7 +45,13 @@ for f in os.listdir(INPUT_PATH):
     if fnmatch.fnmatch(f, '*.[Zz][Ii][Pp]'):
         zips.append(f)
 
+    if fnmatch.fnmatch(f, '*.[Cc][Bb][Zz]'):
+        zips.append(f)
+
     if fnmatch.fnmatch(f, '*.[Rr][Aa][Rr]'):
+        rars.append(f)
+
+    if fnmatch.fnmatch(f, '*.[Cc][Bb][Rr]'):
         rars.append(f)
 
 # Loop over zips & rars, get tags from titles
@@ -43,31 +61,68 @@ for f in os.listdir(INPUT_PATH):
 # Scan individual ZIP file
 for f in zips:
     print "Scanning ZIP: "+f,
-    myzip = zipfile.ZipFile(INPUT_PATH+f, 'r')
+    try:
+        myzip = zipfile.ZipFile(INPUT_PATH+f, 'r')
+    except zipfile.BadZipfile as e:
+        raise Exception(u'"{}" is not a valid ZIP file! Error: {}'.format(f, e))
+    except:
+        raise Exception('Unknown error: {}, ZIP extraction failed: {}').format(e, f)
+
     zip_members = myzip.namelist()
     zip_members = filter(lambda x: is_image(x) is True, zip_members)
     zip_count = zip_members.count()
     print ' done.'
     print "Found {0} members.". format(str(zip_count))
 
-# Add ZIP members to DB
+    # Check if # images > 0
+    if zip_count > 0:
 
-# Spawn greenlet processes to generate thumbnails
+        # Generate MD5 checksum for ZIP file
+        md5 = md5sum(INPUT_PATH+f)
 
-# Display progress bars?
+        # Add volume to DB Volume table
+
+        # Parse tags from filename, add to DB Tags table
+        tags = tag.split_tags(f)
+
+        # Add pages to DB Image table
+        for z in zip_members:
+            pass
+
+        # Spawn greenlet processes to generate thumbnails
+        # Display progress bars?
 
 # Scan individual RAR file
 for f in rars:
     print "Scanning RAR: "+f,
-    myrar = rarfile.RarFile(INPUT_PATH+f, 'r')
+    try:
+        myrar = rarfile.RarFile(INPUT_PATH+f, 'r')
+    except (rarfile.BadRarFile, rarfile.NotRarFile) as e:
+        raise Exception(u'"{}" is not a valid RAR file! Error: {}'.format(f, e))
+    except:
+        raise Exception("Unknown error: {}, RAR extraction failed: {}").format(e, f)
+
     rar_members = myrar.namelist()
     rar_members = filter(lambda x: is_image(x) is True, rar_members)
     rar_count = rar_members.count()
     print ' done.'
     print "Found {0} members.".format(str(rar_count))
 
-# Add RAR members to DB
+    # Check if # images > 0
+    if rar_count > 0:
 
-# Spawn greenlet processes to generate thumbnails
+        # Generate MD5 checksum for RAR file
+        md5 = md5sum(INPUT_PATH+f)
 
-# Display progress bars?
+        # Add volume to DB Volume table
+
+        # Parse tags from filename, add to DB Tags table
+        tags = tag.split_tags(f)
+
+        # Add pages to DB Image table
+        for r in rar_members:
+            pass
+
+        # Spawn greenlet processes to generate thumbnails
+        # Display progress bars?
+
