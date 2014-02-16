c33cfafee89ae863577de6be248ffb93d9de9a3f
Duncan Law-Green <duncan.lawgreen@gmail.com>
2014-02-16 19:15:40 +0000

    Pyparsing to parse archive name to title and tags

--- app/split_tags.py
+++ app/split_tags.py
@@ -5,9 +5,11 @@ Yomiko Comics Reader
 
 import re
 import os.path
+from pyparsing import *
 
 # simple-minded method to separate doujin file names into characteristic tags
 # tried pyparsing but decided wasn't worth the effort
+
 
 def split_tags(data):
 
@@ -28,5 +30,51 @@ def split_tags(data):
 
     # return list of tags
     return tags
+
+
+def split_title_tags(data):
+    # Parse input archive filename
+    # Return title as string, bracketed elements as tag list
+    # Will need to test against Unicode input
+
+    # crop filename extension
+    data = os.path.splitext(os.path.basename(data))[0]
+
+    # substitute left parens with round bracket
+    data = re.sub('[\{\[]', '(', data)
+
+    # substitute right parens with round bracket
+    data = re.sub('[\}\]]', ')', data)
+
+    # Add enclosing brackets to satisfy nestedExpr
+    data = '(' + data + ')'
+
+    # Suppress bracketed sections to extract title
+    # Will need to catch exceptions here
+    title = nestedExpr('(', ')').suppress().transformString(data)
+
+    # Remove whitespace
+    title = title.strip()
+
+    # Remove title from input string
+    data = data.replace(title, '')
+
+    # Substitute all brackets with pipes
+    data = re.sub('[\(\[\)\]\{\}]', '|', data)
+
+    # Split by pipes
+    tags = re.split('\|', data)
+
+    # Strip whitespace from tags
+    tags = [t.strip() for t in tags]
+
+    # Drop empty entries
+    tags = filter(None, tags)
+
+    return title, tags
+
+
+
+
 
 
