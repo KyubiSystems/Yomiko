a99ff7d70f42d2295a895991c3afedba8a3c4bfa
Duncan Law-Green <duncan.lawgreen@gmail.com>
2018-01-06 23:41:56 +0000

    Updated startup

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-Yomiko Comics Reader
-(c) 2016 Kyubi Systems: www.kyubi.co.uk
-"""
-
-from app import app
-#app.run(debug=True)
-app.run(host='0.0.0.0')
---
+++ start.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=app/__init__.py
+export FLASK_DEBUG=1
+flask run
