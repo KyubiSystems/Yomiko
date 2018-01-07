87cd43b401a72375fd8b19446be2f8e2e09e1f50
Duncan Law-Green <duncan.lawgreen@gmail.com>
2018-01-07 20:57:31 +0000

    Code cleanup, adding docstrings

--- app/views.py
+++ app/views.py
@@ -9,8 +9,8 @@ from io import BytesIO
 import rarfile
 from flask import Flask, abort, render_template, send_file
 
-from app.config import *
-from app.models import Volume, Tag, TagRelation, Image, db
+from app.config import INPUT_PATH
+from app.models import Volume, Tag, TagRelation, Image
 
 
 app = Flask(__name__)
@@ -23,7 +23,7 @@ def index():
 # List of titles
 
 @app.route('/view/list')
-def plist():
+def title_list():
 
     vols = Volume.select().order_by(Volume.title)
 
@@ -31,7 +31,7 @@ def plist():
 
 
 @app.route('/view/thumbs')
-def tlist():
+def thumb_list():
 
     vols = Volume.select().order_by(Volume.title)
 
@@ -39,17 +39,23 @@ def tlist():
 
 # ---- Tag operations -----
 
-# Show all tags
 @app.route('/tags')
-def alltags():
+def all_tags():
+
+    """
+    Show all tags
+    """
 
     tags = Tag.select().order_by(Tag.name)
 
     return render_template("tags.html", tags=tags)
 
-# Show titles associated with given tag
 @app.route('/tags/<int:tag_id>')
-def showtag(tag_id):
+def show_tag(tag_id):
+
+    """
+    Show titles associated with given tag tag_id
+    """
 
     try:
         tags = Tag.select().where(Tag.id == tag_id).order_by(Tag.name)
@@ -61,7 +67,10 @@ def showtag(tag_id):
 @app.route('/tags/<int:tag_id>/edit')
 def edit_tag(tag_id):
 
-    # DB query to get specific tag
+    """
+    Edit Tag tag_id in DB
+    """
+
     try:
         tag = Tag.get(id == tag_id)
     except Tag.DoesNotExist:
@@ -72,7 +81,10 @@ def edit_tag(tag_id):
 @app.route('/tags/<int:tag_id>/delete')
 def delete_tag(tag_id):
 
-    # DB query to get specific tag
+    """
+    Delete Tag tag_id from DB
+    """
+
     try:
         tag = Tag.get(id == tag_id)
     except Tag.DoesNotExist:
@@ -80,28 +92,31 @@ def delete_tag(tag_id):
 
     return render_template("tags.html", tags=tag)
 
-# Filter operations
-
 @app.route('/filter/<filter_string>')
 def filter_tags(filter_string):
 
-    # filter string is dot-separated list of tag IDs
+    """
+    Display list of titles filtered by tags
+    filter_string is dot-separated list of tag IDs
+    """
 
     # TODO: Check how to do filter search on titles
 
     return render_template("tags.html")
 
-# Application settings
-
 @app.route('/settings')
 def settings():
-    return render_template("settings.html")
+    """Display application settings page"""
 
-# Render title (grid of thumbnails).
-# Image 0 as blurred CSS background?
+    return render_template("settings.html")
 
 @app.route('/title/<int:title_id>')
 def title(title_id):
+
+    """
+    Render title (grid of thumbnails).
+    Image 0 as blurred CSS background?
+    """
 
     # Get title information
     try:
@@ -116,15 +131,18 @@ def title(title_id):
     tags = Tag.select(Tag, TagRelation, Volume).join(TagRelation).join(Volume).where(Volume.id == title_id).order_by(Tag.name)
     
     # pass list of thumbnails to template
-    return render_template("title.html", title=volume.title, id=str(title_id), thumbs=thumbs, tags=tags)
+    return render_template("title.html", title=volume.title,
+                           id=str(title_id), thumbs=thumbs, tags=tags)
 
 # Render individual page image
 
 @app.route('/page/<int:title_id>/<int:page_num>')
-def page(title_id, page_num):
+def page_image(title_id, page_num):
 
-    # DB query to get archive file corresponding to title ID
-    # and member file corresponding to page number
+    """Get archive file corresponding to title ID
+    and member file corresponding to page number
+    
+    Returns extracted image to browser"""
 
     # Get title information
     try:
@@ -134,7 +152,8 @@ def page(title_id, page_num):
 
     # Get page information
     try:
-        page = Image.select().join(Volume).where((Volume.id == title_id) & (Image.page == page_num)).get()
+        page = Image.select().join(Volume).where((Volume.id == title_id)
+                                                 & (Image.page == page_num)).get()
     except Image.DoesNotExist:
         abort(404)
 
@@ -142,25 +161,25 @@ def page(title_id, page_num):
 
         # TODO: Error check on archive open
         z = zipfile.ZipFile(INPUT_PATH+volume.filename)
-        
+
         # Get page binary data
         # TODO: Error checking
-        foo = z.read(page.filename)
-        
+        zf = z.read(page.filename)
+
         z.close()
-  
-    # Return extracted image to browser
-        return send_file(BytesIO(foo), mimetype=page.mimetype)
+
+        # Return extracted image to browser
+        return send_file(BytesIO(zf), mimetype=page.mimetype)
 
     elif volume.filetype == 'rar':
-    
+
         # TODO: Error check on archive open
         rar = rarfile.RarFile(INPUT_PATH+volume.filename)
-        
+
         # Get page binary data
         # TODO: Error checking
         rardata = rar.read(page.filename)
-        
+
         rar.close()
 
     # Return extracted image to browser
@@ -173,9 +192,10 @@ def page(title_id, page_num):
 # Test image display auto-size (responsive)
 @app.route('/auto/<int:title_id>/<int:page_num>')
 def autotest(title_id, page_num):
+    """Test template for responsive autoscaling, displays Title title_id, page page_num
 
-    # Pass image details to HTML/CSS page which should responsively autoscale
-    # prep for implementation of image slider for page view
+    Pass image details to HTML/CSS page which should responsively autoscale
+    prep for implementation of image slider for page view"""
 
     return render_template("autotest.html", title_id=title_id, page_num=page_num)
 
@@ -183,6 +203,7 @@ def autotest(title_id, page_num):
 # Test Unslider display
 @app.route('/slide/<int:title_id>')
 def slide(title_id):
+    """Test template for Unslider display, displays Title title_id"""
 
     # Get title information
     try:
@@ -191,7 +212,8 @@ def slide(title_id):
         abort(404)
 
     # Get list of images for this title
-    pages = Image.select(Image, Volume).join(Volume).where(Volume.id == title_id).order_by(Image.page)
+    pages = Image.select(Image, Volume).join(Volume).where(
+        Volume.id == title_id).order_by(Image.page)
 
     # Pass volume and page details to Unslider template
     return render_template("slider.html", title_id=title_id, volume=volume, pages=pages)
@@ -200,9 +222,11 @@ def slide(title_id):
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """Error handler for 404 Page not Found"""
     return render_template("404.html"), 404
 
 @app.errorhandler(500)
 def internal_error(error):
+    """Error handler for 500 Internal Error"""
     #db.session.rollback()
     return render_template("500.html"), 500
