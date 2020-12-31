f182f2ee5aa3121cb7fdba46232040285611713e
Duncan Law-Green <duncan.lawgreen@gmail.com>
2020-12-31 23:35:13 +0000

    Add version to Docker build; update requirements

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker build -t kyubi/yomiko:alpine -f Dockerfile.alpine .
+# Get version number
+. ./VERSION
+
+# Build lightweight alpine docker image
+docker build -t kyubi/yomiko:latest -t kyubi/yomiko:v$VERSION -f Dockerfile.alpine .
